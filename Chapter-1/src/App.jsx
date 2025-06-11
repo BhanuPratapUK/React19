e11cf3e9b8bc6dbb7a4d438bf379ec901f41fd2b
Bhanu Pratap <ppratapbhanu0@gmail.com>
2025-06-11 03:26:59 +0100

    How to Send the Props

---
+++ Chapter-1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Components from './Components.jsx'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Components id="1234556" title= "Mahabharta Book" />
+    </>
+  )
+}
+
+export default App
---
+++ Chapter-1/src/Components.jsx
@@ -0,0 +1,9 @@
+export default function Components(x){
+    return (
+        <>
+        <h5>Passing the Prop in to the component the id is {x.id} and the title is {x.title}</h5>
+        <p> Here we have learned how to send props to components id and the title is the Props</p>
+        </>
+    )
+}
+
